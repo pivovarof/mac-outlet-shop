87d4ba43e51e8569b094eccd66634396417a67dd
pivovarof <lgagrog@gmail.com>
2021-11-21 13:16:14 +0200

    finished modal window

--- js/mainjs.js
+++ js/mainjs.js
@@ -5,32 +5,29 @@ function addCards() {
     let name = i.name;
     let inStock = i.orderInfo.inStock;
     let price = i.price;
+    let id = i.id;
 
     cards.insertAdjacentHTML("afterbegin",
-     `  <div class="col-lg-6 col-md-6 mb-4 cards-lok popup-link">
-    <div class="card">
-      <div class="view owerlay" >
-        <img class="card-img-top" src="img/${img}" alt="Apple">
-        <a href="#">
-          <div class="mask rgba-white-slight"></div>
-        </a>
-      </div>
-      <div class="card-body text-center">                      
-        <h3>
-          <strong >
-            <a href="" class="black-text">${name}</a>
-          </strong>
-        </h3>
-        <p class="grey-text fw-bold"><i class="far fa-check-circle text-success"></i>
-          <span class="fw-bold "> ${inStock}</span> left in stock</p>
-                             
-        <button type="button" class="btn btn-primary">Add to cart</button>
-        <h4 class="font-weight-bold mt-3">
-          <strong class="string-price">${price}$</strong>
-        </h4>
-      </div>
-    </div>
-  </div>` )
+     `  <div class="col-lg-6 col-md-6 mb-4 cards-lok popup-link" id="${id}">
+     <div class="card d-flex align-items-center justify-content-center">
+       <div class="view owerlay " style="width: 18rem; height: 18rem;">
+         <img class="card-img-top " src="img/${img}" alt="Apple">      
+       </div>
+       <div class="card-body text-center">                      
+         <h3>
+           <strong class="name_card">${name}</strong>       
+           
+         </h3>
+         <p class="grey-text fw-bold"><i class="far fa-check-circle text-success"></i>
+           <span class="fw-bold "> ${inStock}</span> left in stock</p>
+                              
+         <button type="button" class="btn btn-primary">Add to cart</button>
+         <h4 class="font-weight-bold mt-3">
+           <strong class="string-price">${price}$</strong>
+         </h4>
+       </div>
+     </div>
+   </div>` )
     
 
 
--- js/popaps.js
+++ js/popaps.js
@@ -1,18 +1,44 @@
 const popupLinks = document.querySelectorAll('.popup-link');
 const body = document.querySelector('body');
-let popup = document.getElementsByClassName('popup')
+const popup = document.getElementsByClassName('popup');
+const namePopup = document.getElementsByClassName('name_popup');
+const nameCards = document.getElementsByClassName('name_card');
+const pricePopup = document.getElementsByClassName('price_popup');
+const imgPopup = document.getElementsByClassName('img_popup');
+const stockPopup = document.getElementsByClassName('stock_popup');
+const reviePopup = document.getElementsByClassName('revie_popup');
+const colorPopup = document.getElementsByClassName('color_popup');
+const osPopup = document.getElementsByClassName('os_popup');
+const chipPopup = document.getElementsByClassName('chip_popup');
+const heightPopup = document.getElementsByClassName('height_popup');
+const widthPopup = document.getElementsByClassName('width_popup');
+const depthPopup = document.getElementsByClassName('depth_popup');
+const weightPopup = document.getElementsByClassName('weight_popup');
 
-console.log(popup);
-let card;
+
+
+
 popupLinks.forEach( elem => {
     elem.addEventListener('click', function open(){
-        popup[0].classList.add('open')
+         popup[0].classList.add('open')       
+         namePopup[0].innerHTML = items[elem.id].name;
+         pricePopup[0].innerHTML = `${items[elem.id].price}$`;         
+         imgPopup[0].outerHTML = `<img class="card-img-top img_popup"  src="img/${items[elem.id].imgUrl}" alt="Apple">`;
+         stockPopup[0].innerHTML = items[elem.id].orderInfo.inStock;
+         reviePopup[0].innerHTML = `${items[elem.id].orderInfo.reviews}% `;
+         colorPopup[0].innerHTML = items[elem.id].color;
+         osPopup[0].innerHTML = items[elem.id].os;
+         chipPopup[0].innerHTML = items[elem.id].chip.name;
+         heightPopup[0].innerHTML = `${items[elem.id].size.height} cm`;
+         widthPopup[0].innerHTML = `${items[elem.id].size.width} cm`;
+         depthPopup[0].innerHTML = `${items[elem.id].size.depth} cm`;
+         weightPopup[0].innerHTML = `${items[elem.id].size.weight} kg`;
     });
     
 })
 
 popup.forEach( elem => {
-    elem.addEventListener('click', function clouse(){
+    elem.addEventListener('click', function close(){
         popup[0].classList.remove('open')
     });
     
